fffe4fa14be4c807c0b6577535952ed9ca71ab47
jhan07-sanchez <jhansancheza@gmail.com>
2026-03-14 11:14:31 -0500

    cambios en config

--- config/settings/production.py
+++ config/settings/production.py
@@ -64,10 +64,11 @@ CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 # =====================
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
+        ssl_require=True,
     )
 }
 
